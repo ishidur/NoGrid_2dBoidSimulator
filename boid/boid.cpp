84648064ff5b81c8ee38a292c3edb210e547f66f
ishidu-ryota-fz <ishidu-ryota-fz@ynu.jp>
2017-04-22 22:11:02 +0900

    initial commit

--- boid/boid.cpp
+++ boid/boid.cpp
@@ -2,10 +2,44 @@
 //
 
 #include "stdafx.h"
+#include <GL/glut.h>
 
-
-int main()
+void drawSquare(double x, double y, double width, double height)
 {
-    return 0;
+	glBegin(GL_POLYGON);
+	glVertex2d(x, y);
+	glVertex2d(x + width, y);
+	glVertex2d(x + width, y + height);
+	glVertex2d(x, y + height);
+	glEnd();
 }
 
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3d(1.0, 0.0, 0.0);
+	drawSquare(-1.0, -1.0, 1.0, 1.0);
+	glColor3d(0.0, 1.0, 0.0);
+	drawSquare(0.0, -1.0, 1.0, 1.0);
+	glColor3d(0.0, 0.0, 1.0);
+	drawSquare(-1.0, 0.0, 1.0, 1.0);
+	glColor3d(1.0, 0.0, 1.0);
+	drawSquare(0.0, 0.0, 1.0, 1.0);
+	glFlush();
+}
+
+void init(void)
+{
+	glClearColor(0.0, 0.0, 1.0, 1.0);
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA);
+	glutCreateWindow(argv[0]);
+	glutDisplayFunc(display);
+	init();
+	glutMainLoop();
+	return 0;
+}
